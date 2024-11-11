74c2382e51c4a258f91b993a1ee7aad9dc95d782
mieramensatu <sasakihaise985@gmail.com>
2024-11-12 02:20:45 +0700

    memo with vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,190 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from "vue";
+
+const showForm = ref(false);
+const newMemo = ref("");
+const memos = ref([]);
+const error = ref("");
+
+function addMemo() {
+  if (newMemo.value === "") {
+    error.value = "Please enter a memo";
+    return;
+  }
+  memos.value.push({
+    id: Date.now(),
+    content: newMemo.value,
+    date: new Date().toLocaleString("en-us"),
+    backgroundColor: getRandomColor(),
+  });
+  newMemo.value = "";
+  showForm.value = false;
+}
+
+function deleteMemo(id) {
+  memos.value = memos.value.filter((memo) => memo.id !== id);
+}
+
+function getRandomColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <div class="container">
+      <header>
+        <h1 class="header-title">memo</h1>
+        <button @click="showForm = true" class="header-button">+</button>
+      </header>
+      <div class="card-container">
+        <div
+          v-for="(memo, index) in memos"
+          :key="index"
+          class="card"
+          :style="{ backgroundColor: memo.backgroundColor }"
+        >
+          <p class="card-content">
+            {{ memo.content }}
+          </p>
+          <div class="card-footer">  
+            <p>{{ memo.date }}</p>
+            <button @click="deleteMemo(memo.id)" class="card-button">x</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-if="showForm" class="form-overlay">
+      <div class="form-modal">
+        <button @click="showForm = false" class="form-close-btn">
+          &times;
+        </button>
+        <p v-if="error" class="form-error">{{ error }}</p>
+        <textarea
+          v-model="newMemo"
+          name="memo"
+          id="memo"
+          cols="30"
+          rows="10"
+        ></textarea>
+        <button @click="addMemo" class="form-save-btn">save</button>
+      </div>
+    </div>
   </main>
 </template>
 
 <style scoped>
+main {
+  height: 100vh;
+  width: 100vw;
+}
+.container {
+  max-width: 900px;
+  margin: 0 auto;
+}
 header {
-  line-height: 1.5;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+header .header-title {
+  color: gray;
+  font-size: 3rem;
+  font-weight: bold;
+  margin-bottom: 3rem;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+header .header-button {
+  cursor: pointer;
+  border: transparent;
+  border-radius: 100%;
+  height: 50px;
+  width: 50px;
+  padding: 10px;
+  background-color: cornflowerblue;
+  color: white;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.card {
+  width: 225px;
+  height: 225px;
+  padding: 10px;
+  background-color: #ffa6c1;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+.card-container {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.card-footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.form-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.4);
+  z-index: 10;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.form-modal {
+  width: 420px;
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+}
+
+.form-save-btn {
+  padding: 10px 20px;
+  font-size: 20px;
+  width: 100%;
+  background-color: #495a7d;
+  border: none;
+  border-radius: 5px;
+  color: white;
+  cursor: pointer;
+  margin-top: 20px;
+  box-shadow: 0 0 10px #495a7d;
+  font-weight: bold;
+  transition: all 0.2s ease;
+  outline: none;
+}
+.form-save-btn:hover {
+  background-color: #516393;
+  box-shadow: 0 0 10px #516393;
+  transform: scale(1.03);
+  transition: all 0.2s ease;
+  outline: none;
+}
+
+.form-close-btn {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 30px;
+  height: 30px;
+  background-color: transparent;
+  border: none;
+  font-size: 25px;
+  cursor: pointer;
+}
+.form-error {
+  color: red;
 }
 </style>
